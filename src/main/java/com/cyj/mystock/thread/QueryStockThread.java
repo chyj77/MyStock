b8950e931a6ce9518cbe91c2117e9abb20401f36
kennyjin601 <yongjin@163.com>
2017-10-10 13:31:44 +0800

    增加新的持仓数据

--- src/main/java/com/cyj/mystock/thread/QueryStockThread.java
+++ src/main/java/com/cyj/mystock/thread/QueryStockThread.java
@@ -58,7 +58,7 @@ public class QueryStockThread implements Runnable {
             CloseableHttpClient httpclient = HttpClients.createDefault();
             Map<String, CcgpVO> map = CcgpCache.getAll();
             StringBuffer sb = new StringBuffer();
-            if(map==null||map.size()==0) {
+            if (map == null || map.size() == 0) {
                 System.out.println(" CcgpCache is null !");
                 List<CcgpVO> list = ccgpService.getAll();
 //                    ccgpService.add(list);
@@ -70,9 +70,9 @@ public class QueryStockThread implements Runnable {
                     }
                     sb.append(gp.getCode()).append(",");
                 }
-            }else{
+            } else {
                 System.out.println(" CcgpCache is not null !");
-                for (String key:map.keySet()){
+                for (String key : map.keySet()) {
                     if (key.startsWith("00") || key.startsWith("30")) {
                         sb.append("sz");
                     } else {
@@ -89,55 +89,60 @@ public class QueryStockThread implements Runnable {
                 RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(30000).setConnectTimeout(15000)
                         .setConnectionRequestTimeout(30000).build();
                 request.setConfig(requestConfig);
+                String content = null;
+                try {
+                    CloseableHttpResponse response = httpclient.execute(request);
+                    org.apache.http.Header[] headers = response.getHeaders("Content-Type");
 
-                CloseableHttpResponse response = httpclient.execute(request);
-                org.apache.http.Header[] headers = response.getHeaders("Content-Type");
-
-                String content = EntityUtils.toString(response.getEntity());
+                    content = EntityUtils.toString(response.getEntity());
 //                System.out.println(date + "-->content:" + content);
-                String[] stockhqs = content.split(";");
-                for (String stockhqstr : stockhqs) {
-                    if (StringUtils.isNotBlank(stockhqstr)) {
-                        String[] stockhqstrs = stockhqstr.split(",");
-                        String stockCode = stockhqstrs[0].trim().substring(13, 19);
-                        String nowprice = stockhqstrs[3].trim();
-                        CcgpVO ccgpVO = ccgpService.get(stockCode);
-                        String rq = ccgpVO.getRq();
-                        Date ccrq = sdf.parse(rq);
-                        int ccday = daysBetween(ccrq,now);
-                        ccgpVO.setCcday(String.valueOf(ccday));
-                        ccgpVO.setNowprice(nowprice);
-                        String sl = ccgpVO.getSl();
-                        BigDecimal d_sl = new BigDecimal(1);
-                        BigDecimal d_nowprice = new BigDecimal(0);
-                        BigDecimal d_buyprice = new BigDecimal(0);
-                        BigDecimal d_yke = new BigDecimal(0);
-                        BigDecimal d_zdl = new BigDecimal(0);
-                        if (StringUtils.isNotBlank(sl)) {
-                            d_sl = new BigDecimal(sl);
+                    String[] stockhqs = content.split(";");
+                    for (String stockhqstr : stockhqs) {
+                        if (StringUtils.isNotBlank(stockhqstr)) {
+                            String[] stockhqstrs = stockhqstr.split(",");
+                            String stockCode = stockhqstrs[0].trim().substring(13, 19);
+                            String nowprice = stockhqstrs[3].trim();
+                            CcgpVO ccgpVO = ccgpService.get(stockCode);
+                            String rq = ccgpVO.getRq();
+                            Date ccrq = sdf.parse(rq);
+                            int ccday = daysBetween(ccrq, now);
+                            ccgpVO.setCcday(String.valueOf(ccday));
+                            ccgpVO.setNowprice(nowprice);
+                            String sl = ccgpVO.getSl();
+                            BigDecimal d_sl = new BigDecimal(1);
+                            BigDecimal d_nowprice = new BigDecimal(0);
+                            BigDecimal d_buyprice = new BigDecimal(0);
+                            BigDecimal d_yke = new BigDecimal(0);
+                            BigDecimal d_zdl = new BigDecimal(0);
+                            if (StringUtils.isNotBlank(sl)) {
+                                d_sl = new BigDecimal(sl);
+                            }
+                            if (StringUtils.isNotBlank(nowprice)) {
+                                d_nowprice = new BigDecimal(nowprice);
+                            }
+                            String buyprice = ccgpVO.getBuyprice();
+                            if (StringUtils.isNotBlank(buyprice)) {
+                                d_buyprice = new BigDecimal(buyprice);
+                            }
+                            d_yke = (d_nowprice.subtract(d_buyprice)).multiply(d_sl).setScale(3);
+                            d_zdl = (d_nowprice.subtract(d_buyprice)).divide(d_buyprice, 3, RoundingMode.HALF_UP).multiply(BigDecimal.valueOf(100)).setScale(3);
+                            ccgpVO.setYke(d_yke.toString());
+                            ccgpVO.setZdl(d_zdl.toString());
+                            ccgpService.update(ccgpVO);
+                            JSONObject jsonObject = JSONObject.fromObject(ccgpVO);
+                            String temp = jsonObject.toString();
+                            sendListener.send(jsonObject.toString());
                         }
-                        if (StringUtils.isNotBlank(nowprice)) {
-                            d_nowprice = new BigDecimal(nowprice);
-                        }
-                        String buyprice = ccgpVO.getBuyprice();
-                        if (StringUtils.isNotBlank(buyprice)) {
-                            d_buyprice = new BigDecimal(buyprice);
-                        }
-                        d_yke = (d_nowprice.subtract(d_buyprice)).multiply(d_sl).setScale(3);
-                        d_zdl = (d_nowprice.subtract(d_buyprice)).divide(d_buyprice,3, RoundingMode.HALF_UP).multiply(BigDecimal.valueOf(100)).setScale(3);
-                        ccgpVO.setYke(d_yke.toString());
-                        ccgpVO.setZdl(d_zdl.toString());
-                        ccgpService.update(ccgpVO);
-                        JSONObject jsonObject = JSONObject.fromObject(ccgpVO);
-                        String temp = jsonObject.toString();
-                        sendListener.send(jsonObject.toString());
                     }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
                 if (!IsBreak) {
                     System.out.println("donot see me !");
                     break;
                 }
-                Thread.sleep(1000*2);
+                Thread.sleep(1000 * 2);
             }
             System.out.println(new Date() + " 关闭了查询股票行情接口!");
         } catch (Exception e) {
@@ -145,17 +150,16 @@ public class QueryStockThread implements Runnable {
         }
     }
 
-    public  int daysBetween(Date ccrq,Date now) throws ParseException
-    {
-        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
-        ccrq=sdf.parse(sdf.format(ccrq));
-        now=sdf.parse(sdf.format(now));
+    public int daysBetween(Date ccrq, Date now) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        ccrq = sdf.parse(sdf.format(ccrq));
+        now = sdf.parse(sdf.format(now));
         Calendar cal = Calendar.getInstance();
         cal.setTime(ccrq);
         long time1 = cal.getTimeInMillis();
         cal.setTime(now);
         long time2 = cal.getTimeInMillis();
-        long between_days=(time2-time1)/(1000*3600*24);
+        long between_days = (time2 - time1) / (1000 * 3600 * 24);
         return Integer.parseInt(String.valueOf(between_days));
     }
 }
